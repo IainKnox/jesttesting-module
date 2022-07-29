24befec9ffc14342a924af41f32f48d83b1c57d2
Iain Knox <iain.knox1@btinternet.com>
2022-07-29 16:10:03 +0000

    create test to test if score key exists

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,7 +1,17 @@
+const expectExport = require("expect");
+const { test } = require("picomatch");
+const { describe } = require("yargs");
+
 beforeAll(() => {
-    let fs = require((fs));
+    let fs = require(('fs'));
     let fileContents = fs.readFileSync("simon-game.html", 'utf-8');
     document.open();
     document.write(fileContents);
     document.close;
 });
+
+describe("game object contains correct keys", () => {
+    test('score key exists', () => {
+        expect('score' in game).toBe(true);
+    });
+})
